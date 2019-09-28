7fe393a488bae3386fecf16b8d0e5f41e1bf155b
João Victor Claudino Felipe <victorskg@outlook.com>
2019-09-28 14:11:04 -0300

    Inicio da lógica dos tiros

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+
+    private Vector3 lastDirection;
+    private Vector3 bulletDirection;
+
+    void Start() {
+        lastDirection = new Vector3(-1, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        UpdateBulletDirection();
+        if (Input.GetKeyDown("z")) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(lastDirection * bulletForce, ForceMode2D.Impulse);
+    }
+
+    void UpdateBulletDirection() {
+        bulletDirection.x = Input.GetAxisRaw("Horizontal");
+        bulletDirection.y = Input.GetAxisRaw("Vertical");
+
+        if (bulletDirection != Vector3.zero) {
+            lastDirection = bulletDirection;
+        }
+    }
+}
