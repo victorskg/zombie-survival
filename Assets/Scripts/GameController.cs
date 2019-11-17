07b63e701d1b0dff6d1aed825e43261d2c0ace2a
João Victor Claudino Felipe <victorskg@outlook.com>
2019-11-17 20:41:16 -0300

    Criação das waves de zumbi

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    
+    public int currentWave;
+    public int zombieIncrement;
+    public GameObject[] zombies;
+    public GameObject zombiePrefab;
+
+    void Start() {
+        currentWave = 1;
+        zombieIncrement = 2;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        zombies = GameObject.FindGameObjectsWithTag("Zombie");
+        ControlWave();
+    }
+
+    void ControlWave() {
+        if (zombies.Length == 0) {
+            currentWave++;
+            NextWave();
+        }
+    }
+
+    void NextWave() {
+        for (int i = 0; i < currentWave + zombieIncrement; i++) {
+            Instantiate(zombiePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+}
