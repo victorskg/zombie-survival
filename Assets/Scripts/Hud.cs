401fdf37008197c0e8da709d601bd3ce4f163693
Samuel Souza <samuelgsete@gmail.com>
2019-11-21 15:16:02 -0300

    feature/hud Ajustes na rud, attack zumbe e dano no player

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+    private PlayerLife playerLife;
+    public Sprite[] bar;
+    private Image healtbar;
+    private Text cronometro;
+    private float timer = 0f;
+
+    void Start()
+    {
+        playerLife = GameObject.Find("Player").GetComponent<PlayerLife>();
+        healtbar = GameObject.FindGameObjectWithTag("Hud").GetComponentInChildren<Image>();
+        cronometro = GameObject.FindGameObjectWithTag("Hud").GetComponentInChildren<Text>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        healtbar.sprite = bar[playerLife.getHp()/playerLife.getDano()];
+        cronometro.text = "Tempo decorrido: " + timer.ToString("F0") + "s";
+    }
+}
---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    private int hp = 100;
+    private int dano = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public int sofrerDano()
+    {
+        if(dano <= hp)
+        {
+            hp -= dano;
+            return hp;
+        }
+        return 0;
+    }
+
+    public int getHp()
+    {
+        return hp;
+    }
+
+    public int getDano()
+    {
+        return dano;
+    }
+}
