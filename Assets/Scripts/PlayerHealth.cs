200b1ba2bf09179aacb48c8bec38e170c715d1e8
João Victor Claudino Felipe <victorskg@outlook.com>
2019-11-23 13:49:56 -0300

    Melhorias e correção da camera

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    private int health = 100;
+    private int zombieDamage = 10;
+
+    void Start() {
+        health = 100;
+        zombieDamage = 10;
+    }
+
+    public int TakeDamage() {
+        health -= zombieDamage;
+        
+        if (health < 0) {
+            health = 0;
+        }
+        
+        return health;
+    }
+
+    public int GetHealth() {
+        return health;
+    }
+
+    public int GetDamage() {
+        return zombieDamage;
+    }
+}
--- Assets/Scripts/ZombieHandler.cs
+++ Assets/Scripts/ZombieHandler.cs
@@ -4,21 +4,58 @@ using UnityEngine;
 
 public class ZombieHandler : MonoBehaviour {
 
+    private Transform player;
+    private Animator animator;
+    private float limit = 1.4f;
     public HealthBar healthBar;
+    private Rigidbody2D myRigidbody;
     public HealthSystem healthSystem;
-    
+    private PlayerHealth playerHealth;
+
     void Start() {
         this.healthSystem = new HealthSystem(100.0f);
         this.healthBar.Setup(this.healthSystem);
+
+        this.animator = this.GetComponent<Animator>();
+        this.myRigidbody = GetComponent<Rigidbody2D>();
+        this.player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        this.playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    void Update() {
+        HandlePlayerPosition();
     }
 
     void OnCollisionEnter2D(Collision2D col) {
-        if(col.gameObject.tag == "Bullet") {
-            healthBar.healthSystem.Damage(10.0f);
+        switch(col.gameObject.tag) {
+            case "Bullet":
+                HandleBulletCollision(col);
+                break;
+            case "Player":
+                HandlePlayerCollision(col);
+                break;
+            default: break;
         }
+    }
 
-        if(healthBar.healthSystem.GetHealth() == 0.0f) {
+    void HandlePlayerPosition() {
+        Vector3 distance = (Vector2) player.position - myRigidbody.position;
+        if (Mathf.Abs(distance.x) >= limit || Mathf.Abs(distance.y) >= limit) {
+            animator.SetBool("attack", false);
+        }
+    }
+
+    void HandleBulletCollision(Collision2D col) {
+        healthBar.healthSystem.Damage(10.0f);
+        if (healthBar.healthSystem.GetHealth() == 0.0f) {
             Destroy(gameObject);
         }
+    } 
+
+    void HandlePlayerCollision(Collision2D col) {
+        if (animator.gameObject.activeSelf){
+            animator.SetBool("attack", true);
+        }
+        playerHealth.TakeDamage();
     }
 }
