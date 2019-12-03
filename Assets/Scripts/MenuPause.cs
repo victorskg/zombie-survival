46c5a432f05de8d7ea46fed5ab27a1a0aec3ee82
Samuel Souza <samuelgsete@gmail.com>
2019-12-03 15:50:10 -0300

    feature/interface criação da interface do jogo

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public GameObject menuPauseUi;
+    private static bool gameIsPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+    public void resume()
+    {
+        menuPauseUi.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void pause()
+    {
+        menuPauseUi.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void sairParaMenu()
+    {
+        SceneManager.LoadScene("Interface");
+        resume();
+    }
+
+    public void reiniciarGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+        resume();
+    }
+}
